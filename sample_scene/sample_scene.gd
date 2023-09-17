426a9f62cf0ed9b2008fda7fe6a9b5ae2c1a83d9
DanielSnd <daniel.snd.anim@gmail.com>
2023-09-17 18:39:49 -0300

    Added error checking to the sample scene

--- sample_scene/sample_scene.gd
+++ sample_scene/sample_scene.gd
@@ -6,5 +6,14 @@ extends Node2D
 func _ready():
 	# Here we fetch the item branch from the database
 	var branch_item := Database.get_item("branch")
+	
+	# Here we check if we got an error. If the database can't find the item it returns a default item with error bool flagged to true.
+	if branch_item.error:
+		print("Error fetching item branch")
+		return
+		
+	# Here we print the item description to the console
+	print(branch_item.description)
+	
 	# Here we set the icon from the item branch to our sprite 2d
 	sprite_2d.texture = branch_item.get_texture()
